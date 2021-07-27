3844748d7fe5f820c439059e9f2f07b626f804fa
Daniel Ribeiro de Oliveira <daniel.ribeiro.dev@outlook.com>
2021-07-27 13:32:28 -0300

    Configurando eventos ao mover a peça do jogador sobre a tabela

--- src/js/script.ts
+++ src/js/script.ts
@@ -1,16 +1,37 @@
-// HTML Elements
+// ---- HTML Elements ----
 let darkModeButton = document.querySelector('header .dark-mode-button') as HTMLButtonElement;
+let playerElement = document.querySelector('.player-area .player') as HTMLParagraphElement;
+let table = document.querySelector('.table') as HTMLDivElement;
 
-// Initial Data
+// ---- Initial Data ----
 const darkMode = {
     status: false,
     toggle
 }
 
-// Events
-    darkModeButton.addEventListener('click', darkMode.toggle);
+let player: string = ( Math.round(Math.random()) ) ? 'X' : 'O';
+let gameStatus: boolean = true;
 
-// Functions
+renderInfo();
+
+
+// ---- Events ----
+// dark mode
+darkModeButton.addEventListener('click', darkMode.toggle);
+
+// player 
+playerElement.addEventListener('dragstart', dragStartPlayerElement);
+playerElement.addEventListener('dragend', dragEndPlayerElement);
+
+// table
+table.addEventListener('dragover', dragOverTable);
+table.addEventListener('dragleave', dragLeaveTable);
+table.addEventListener('drop', dropOnTable);
+
+
+
+// ---- Functions ----
+// dark mode
 function toggle(e: Event) {
     let className = (darkMode.status) ? 'dark-mode' : 'light-mode';
     let elements = document.querySelectorAll('.' + className);
@@ -24,3 +45,52 @@ function toggle(e: Event) {
     darkModeIndicatorElement.classList.toggle('active');
     darkMode.status = (darkMode.status) ? false : true;
 }
+
+// player
+function dragStartPlayerElement(e: Event) {
+    let element = e.currentTarget as HTMLParagraphElement;
+    
+    element.classList.add('dragging');
+}
+function dragEndPlayerElement(e: Event) {
+    let element = e.currentTarget as HTMLParagraphElement;
+    element.classList.remove('dragging');
+}
+
+//table
+function dragOverTable(e: Event) {
+    let element = e.target as HTMLDivElement;
+    
+    if(element.innerHTML || gameStatus === false) {
+        return;
+    }
+    
+    e.preventDefault();
+    element.classList.add('overOption');
+}
+function dragLeaveTable(e: Event) {
+    let element = e.target as HTMLDivElement;
+    element.classList.remove('overOption');
+}
+function dropOnTable(e: Event) {
+    let element = e.target as HTMLDivElement;
+    
+    element.classList.remove('overOption');
+    element.innerHTML = player;
+
+    player = (player === 'X') ? 'O' : 'X';
+    renderInfo();
+}
+
+// info
+function renderInfo() {
+    if(gameStatus === false) {
+        playerElement.setAttribute('draggable', 'false');
+    } else {
+        playerElement.setAttribute('draggable', 'true');
+    }
+
+    playerElement.innerHTML = player;
+}
+
+// validations 
